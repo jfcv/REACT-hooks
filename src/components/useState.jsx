8d7fcf3154ca82b6fa9f1ef3a059d9c20d8a4d38
Juan Cuadros <juan.cuadros@pm.me>
2022-03-28 17:32:05 -0500

    Implement useEffect hook showing its behavior alone and with the dependencies array.

--- src/components/useState.jsx
+++ src/components/useState.jsx
@@ -1,20 +1,29 @@
-import { toHaveFormValues } from "@testing-library/jest-dom/dist/matchers";
-import React, { Component, useState } from "react";
+import React, { useEffect } from "react";
 import { useForm } from "./useForm";
 
 const UseState = () => {
   const [values, handleChange] = useForm({ email: "", password: "" });
 
+  /**
+   * every time the components gets re-render the
+   * useEffect function gets called
+   */
+  useEffect(() => {
+    console.log("useEffect hook!");
+  }, [values.email]);
+
   return (
     <div>
       <input
         type="text"
+        placeholder="email"
         name="email"
         value={values.email}
         onChange={handleChange}
       />
       <input
         type="password"
+        placeholder="password"
         name="password"
         value={values.password}
         onChange={handleChange}
